29bd15e73a840c19e6c182c2429758ada07553dd
Jasper <jasper.tarvainen@student.lut.fi>
2025-10-28 11:22:05 +0200

    Added half of the coursework on Express-Crash

---
+++ Coursework/Express-Crash/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import path from 'path';
+import posts from './routes/posts.js';
+const port = process.env.PORT || 7000;
+
+const app = express();
+
+
+// Setup static folder
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/api/posts', posts);
+
+app.listen(port, () => console.log(`Server is running on port: ${port}`));

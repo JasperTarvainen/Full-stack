2b874e73bb17aeda0b4dd47c1880843359df0414
Jasper <jasper.tarvainen@student.lut.fi>
2025-10-30 23:59:37 +0200

    more files

--- Mern_stack/backend/controllers/goalController.js
+++ Mern_stack/backend/controllers/goalController.js
@@ -1,31 +1,33 @@
+const asyncHandler = require('express-async-handler')
 // @desc    Get goals
 // @route   GET /api/goals
 // @acces   Private
-const getGoals = async(req, res) => {
+const getGoals = asyncHandler(async(req, res) => {
     res.status(200).json({message: 'Get goals'})
-}
+})
 // @desc    Set goals
 // @route   POST /api/goals
 // @acces   Private
-const setGoal = async(req, res) => {
+
+const setGoal = asyncHandler(async(req, res) => {
     if (!req.body.text) {
         res.status(400)
         throw new Error ('Please add a text field')
     }
     res.status(200).json({message: 'Set goal'})
-}
+})
 // @desc    update goals
 // @route   PUT /api/goals/:id
 // @acces   Private
-const updateGoal = async(req, res) => {
+const updateGoal = asyncHandler(async(req, res) => {
     res.status(200).json({message: `Update goal ${req.params.id}`})
-}
+})
 // @desc    Delete goals
 // @route   DELETE /api/goals
 // @acces   Private
-const deleteGoal = async(req, res) => {
+const deleteGoal = asyncHandler(async(req, res) => {
     res.status(200).json({message: `Delete goal ${req.params.id}`})
-}
+})
 
 
 module.exports = {

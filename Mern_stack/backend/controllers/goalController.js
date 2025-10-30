529191d2ad97a2d08a3a95dacf05541b76575dd2
Jasper <jasper.tarvainen@student.lut.fi>
2025-10-30 20:50:54 +0200

    added more files

--- Mern_stack/backend/controllers/goalController.js
+++ Mern_stack/backend/controllers/goalController.js
@@ -1,25 +1,29 @@
 // @desc    Get goals
 // @route   GET /api/goals
 // @acces   Private
-const getGoals = (req, res) => {
+const getGoals = async(req, res) => {
     res.status(200).json({message: 'Get goals'})
 }
 // @desc    Set goals
 // @route   POST /api/goals
 // @acces   Private
-const setGoal = (req, res) => {
+const setGoal = async(req, res) => {
+    if (!req.body.text) {
+        res.status(400)
+        throw new Error ('Please add a text field')
+    }
     res.status(200).json({message: 'Set goal'})
 }
 // @desc    update goals
 // @route   PUT /api/goals/:id
 // @acces   Private
-const updateGoal = (req, res) => {
+const updateGoal = async(req, res) => {
     res.status(200).json({message: `Update goal ${req.params.id}`})
 }
 // @desc    Delete goals
 // @route   DELETE /api/goals
 // @acces   Private
-const deleteGoal = (req, res) => {
+const deleteGoal = async(req, res) => {
     res.status(200).json({message: `Delete goal ${req.params.id}`})
 }
 
--- Mern_stack/backend/routes/goalRoutes.js
+++ Mern_stack/backend/routes/goalRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const {getGoals, setGoal, updateGoal, deleteGoal} = require('../controllers/goalController.js')
 
+
 router.route('/').get(getGoals).post(setGoal)
 router.route('/:id').delete(deleteGoal).put(updateGoal)
 

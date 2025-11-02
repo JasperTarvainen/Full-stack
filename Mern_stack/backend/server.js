45ee0925ef4480ea7c913b999563d76f27f166fd
Jasper <jasper.tarvainen@student.lut.fi>
2025-11-03 00:15:47 +0200

    Coursework on tutoria series part3

--- Mern_stack/backend/server.js
+++ Mern_stack/backend/server.js
@@ -2,11 +2,13 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware.js')
 const connectDB = require('./config/db.js')
+const cors = require('cors')
 const port = process.env.PORT  || 5000
 
 connectDB();
 
 const app = express()
+app.use(cors());
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
